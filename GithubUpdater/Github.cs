f379935f18a91e8ed5c7079d83931cbd04b9d752
Jon Graf <jongraf1@gmail.com>
2016-02-08 21:23:32 -0600

    Initial setup with some working code and tests for github

---
+++ GithubUpdater/Github.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GithubUpdater
+{
+    public class Github
+    {
+        string _commandArg;
+        private string _workingDirectory;
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void ExecuteCommand()
+        {
+            using (var proc = new Process())
+            {
+                var processStartInfo = new ProcessStartInfo("Git.exe", _commandArg)
+                {
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    RedirectStandardError = true,
+                    WorkingDirectory = _workingDirectory
+                };
+            }
+        }
+    }
+}
---
+++ GithubUpdaterTest/GithubTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GithubUpdater
+{
+    [TestFixture]
+    public class GithubTest
+    {
+        private Github github = new Github();
+        [Test]
+        public string ShouldPushToGithub()
+        {
+            var clone = github.Clone();
+            return "";
+        }
+    }
+}
